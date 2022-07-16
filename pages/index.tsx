527e2470c69a735f4d2e62c11d1452859151b146
Derek Rodriguez <drod2169@gmail.com>
2022-07-16 09:56:40 -0400

    Authenticated handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { Session } from '@supabase/supabase-js'
 import Account from '../components/account'
-import { supabase } from '../utils/supabase-client'
+import enforceAuthenticated from '../utils/auth'
 
 
 export default function Home({ session }: { session: Session }) {
@@ -9,12 +9,4 @@ export default function Home({ session }: { session: Session }) {
   )
 }
 
-export async function getServerSideProps({ req }) {
-    const { user } = await supabase.auth.api.getUserByCookie(req);
-
-    if (!user) {
-        return { props: {}, redirect: { destination: '/login' } };
-    }
-
-    return { props: {} };
-}
+export const getServerSideProps = enforceAuthenticated()
