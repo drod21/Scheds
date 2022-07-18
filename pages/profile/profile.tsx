9d5ad56f5662ec1afd02e1c03d9ba94c63821cd5
Derek Rodriguez <drod2169@gmail.com>
2022-07-18 08:48:01 -0400

    Fix up auth

--- pages/profile/profile.tsx
+++ pages/profile/profile.tsx
@@ -1,8 +1,82 @@
+import { useEffect, useState } from 'react'
 import { Session } from '@supabase/supabase-js'
-import Account from '../../components/account'
-import { enforceAuthenticated } from '../../utils/enforceAuthenticated'
+import { supabase } from '../../utils/supabase-client'
 
-export default function Profile(props: { session: Session }) {
-	return <Account session={props.session} />
+export default function Profile({ session }: { session: Session }) {
+	const [loading, setLoading] = useState(true)
+	const [username, setUsername] = useState(null)
+
+	useEffect(() => {
+		getProfile()
+	}, [session])
+
+	async function getProfile() {
+		try {
+			setLoading(true)
+			const user = supabase.auth.user()
+
+			let { data, error, status } = await supabase.from('profiles').select(`username`).eq('id', user.id).single()
+
+			if (error && status !== 406) {
+				throw error
+			}
+
+			if (data) {
+				setUsername(data.username)
+			}
+		} catch (error) {
+			alert(error.message)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	async function updateProfile({ username }) {
+		try {
+			setLoading(true)
+			const user = supabase.auth.user()
+
+			const updates = {
+				id: user.id,
+				username,
+				updated_at: new Date(),
+			}
+
+			let { error } = await supabase.from('profiles').upsert(updates, {
+				returning: 'minimal', // Don't return the value after inserting
+			})
+
+			if (error) {
+				throw error
+			}
+		} catch (error) {
+			alert(error.message)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	return (
+		<div className='form-widget'>
+			<div>
+				<label htmlFor='email'>Email</label>
+				<input id='email' type='text' value={session?.user?.email} disabled />
+			</div>
+			<div>
+				<label htmlFor='username'>Name</label>
+				<input id='username' type='text' value={username || ''} onChange={(e) => setUsername(e.target.value)} />
+			</div>
+			<div>
+				<button className='button block primary' onClick={() => updateProfile({ username })} disabled={loading}>
+					{loading ? 'Loading ...' : 'Update'}
+				</button>
+			</div>
+
+			<div>
+				<button className='button block' onClick={() => supabase.auth.signOut()}>
+					Sign Out
+				</button>
+			</div>
+		</div>
+	)
 }
-export const getServerSideProps = enforceAuthenticated()
