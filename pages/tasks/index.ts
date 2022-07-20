23893c5122aad6bd2abda1db24d8e53f2735ba05
Derek Rodriguez <drodriguez@corestream.com>
2022-07-20 09:04:38 -0400

    add actions , fix tasks fetch

--- pages/tasks/index.ts
+++ pages/tasks/index.ts
@@ -2,22 +2,13 @@ import { supabase } from '../../utils/supabase-client'
 
 export { default } from './tasks'
 
-export async function getStaticProps(req) {
-	// Call an external API endpoint to get posts
+export async function getServerSideProps({ req, res }) {
 	const { user } = await supabase.auth.api.getUserByCookie(req)
-	console.log(user, req)
-	const { data: tasks, error } = await supabase
-		.from('tasks')
-		.select('*')
-		.filter('user_id', 'eq', user?.id ?? '')
 
-	console.log({ tasks })
-
-	// By returning { props: { posts } }, the Blog component
-	// will receive `posts` as a prop at build time
-	return {
-		props: {
-			tasks,
-		},
+	if (!user) {
+		return res.status(401).send('Unathorized')
 	}
+	const { data: tasks } = await supabase.from('tasks').select('*').eq('user_id', user?.id)
+
+	return { props: { tasks } }
 }
