f84b7b182af6de737d22c9ebdbbbd5845ce5908c
Derek Rodriguez <drod2169@gmail.com>
2022-07-22 07:13:37 -0400

    Add enums

---
+++ types/enums.ts
@@ -0,0 +1,12 @@
+export enum Size {
+	Small = 'small',
+	Medium = 'medium',
+	Large = 'large',
+}
+
+export enum Status {
+	ToDo = 'todo',
+	Open = 'open',
+	InProgress = 'in progress',
+	Completed = 'completed',
+}
