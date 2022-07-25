a5768870a45703bc5234d38cb28c00e90c4a2676
Derek Rodriguez <drod2169@gmail.com>
2022-07-25 07:31:00 -0400

    Enable server components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	experimental: {
+		concurrentFeatures: true,
+		serverComponents: true,
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
